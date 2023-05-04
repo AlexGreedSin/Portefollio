33b7b67e86a8a979f5a4571780dca34f8447c6dc
AlexGreedSin <74626303+AlexGreedSin@users.noreply.github.com>
2023-05-04 18:36:41 -0400

    Add files via upload

---
+++ CodeL.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import styles from 'styles/Jeux.module.css'
+import CodeL1 from 'public/img/CodeL1.png'
+import CodeL2 from 'public/img/CodeL2.png'
+import CodeL3 from 'public/img/CodeL3.png'
+import Head from "next/head"
+export default function CodeL(){
+    return <>
+        <Head>
+            <title>Portefolio Alexandre Bailey-Proulx Code L</title>
+            <meta name="description" content="Jeu video Code L sur le logiciel Unity Objectif sortir du labyrinthe"/>
+            
+            <meta property="og:title" content="Portefolio Alexandre Bailey-Proulx Code L"/>
+            <meta property="og:description" content="Jeu video Code L sur le logiciel Unity Objectif sortir du labyrinthe"/>
+            <meta property="og:image" content ="http://localhost:3000/public/img/CodeL2.PNG"/>
+        </Head>
+        <h1 className={styles.titre}>Code L</h1>
+
+        <Image src={CodeL1} alt="Image1Code L" className={styles.image} priority/>
+
+        <p className={styles.paragraphe}>Code L est un jeu vidéo crée par moi et deux collègues de classe.
+        Le jeux video est écrit en c# dans le logiciel Unity, un logiciel très populaire
+        pour le développement de jeux professionnels.
+        </p>
+        <Image src={CodeL2} alt="Image2Code L" className={styles.image} priority/>
+        <p className={styles.paragraphe}>
+            J&apos;ai programmé plusieurs choses durant le développement du jeu entre autres : les mouvements possibles du personnage,
+            les collisions de celui-ci avec le background ou le forground 
+            et les animations. De plus, j&apos;ai aussi programmé les changements de scènes ainsi que les énigmes présents dans le jeu. 
+            J&apos;ai aussi fait le design des puzzles et des scènes. J&apos;ai aussi complètement créé et programmé la page 
+            du menu principal. Celle-ci affiche plusieurs items :  le boutton &quot;jouer&quot;, qui permet de commencer le jeu et de, bien 
+            entendu, jouer, le boutton &quot;quitter&quot; qui ferme le tout. J&apos;ai également donner un choix d&apos;option auquel on accède avec le boutton &quot;option&quot;,
+            dont: le choix de graphique, taille de l&apos;écran ainsi que le contrôle du volume.
+        </p>
+        <Image src={CodeL3} alt="Image3Code L" className={styles.image} priority/>
+    </>
+}
---
+++ SpaceSurvivor.jsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import styles from 'styles/Jeux.module.css'
+import SpaceSurvivor1 from 'public/img/SoloGame1.png'
+import SpaceSurvivor2 from 'public/img/SoloGame2.png'
+import Head from "next/head"
+export default function SpaceSurvivor(){
+    return <>
+        <Head>
+            <title>Portefolio Alexandre Bailey-Proulx Space Survivor</title>
+            <meta name="description" content="Jeu video Space Survivor sur le logiciel GameMaker objectif de survivre les asteroid"/>
+        
+            <meta property="og:title" content="Portefolio Alexandre Bailey-Proulx Space Survivor"/>
+            <meta property="og:description" content="Jeu video Space Survivor sur le logiciel GameMaker objectif de survivre les asteroid"/>
+            <meta property="og:image" content ="http://localhost:3000/public/img/SoloGame1.PNG"/>
+        </Head>
+        <h1 className={styles.titre}>Space Survivor</h1>
+
+        <Image src={SpaceSurvivor1} alt="Image1 SpaceSurvivor" className={styles.image} priority/>
+
+        <p className={styles.paragraphe}>Space Survivor est un jeu que j&apos;ai créé complètement seul. J&apos;ai programmer celui-ci en c# 
+        dans un logiciel appellé &quot;GameMaker Studio 2&quot; qui est un logiciel permettant de trier des sprites, objets, scripts, rooms, etc 
+        très simplement et de mettre les codes bien séparé dans chacun d&apos;entre eux. C&apos;est un logiciel très utile pour utiliser et améliorer nos connaissances en programmation 
+        </p>
+        <Image src={SpaceSurvivor2} alt="Image2 SpaceSurvivor" className={styles.image} priority/>
+        <p className={styles.paragraphe}>
+            Dans ce jeu, j&apos;ai tout programmé: le contrôle du vaisseau principal permettant de se déplacer partout sur l&apos;écran, 
+            le tir du véhicule, les possibilité aléatoires de la taille et de la direction des météores, les collisions des tirs sur les ennemis 
+            et sur le joueur. J&apos;ai également programmé les dommages causés par les météores, le delai de tir des ennemis et du boss, 
+            la barre de vie du joueur qui change selon la vie du vaisseau principal, une fonction de sauvegarde automatique lors du changement de niveau, 
+            le menu complet avec l&apos;option de mettre le jeu en plein écran ou non, le bouton pour commencer une nouvelle partie ainsi qu&apos;un bouton qui permet de 
+            retourner a un ancien niveau que le joueur aura sauvegardé la dernière fois. De plus, j&apos;ai aussi fait les designs de tout les sprites dans le jeu.
+            J&apos;ai, bien entendu, programmé l&apos;utilisation de tout les sons comme les tirs des vaisseaux, l&apos;impact des météores et la musique d&apos;ambiance.            
+        </p>
+    </>
+}
